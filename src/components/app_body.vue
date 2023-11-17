69ed2d14508c2be53c34134589290da34fb668a1
danielplebani <danielpleba03@gmail.com>
2023-11-17 19:11:42 +0100

    remove paginate

--- src/components/app_body.vue
+++ src/components/app_body.vue
@@ -47,22 +47,5 @@ export default {
       </div>
     </div>
   </div>
-
-  <nav class="container mb-5 d-flex justify-content-center" aria-label="Page navigation example">
-    <ul class="pagination pagination-sm">
-      <li class="page-item">
-        <a class="page-link" href="#" aria-label="Previous">
-          <span aria-hidden="true">Previous</span>
-        </a>
-      </li>
-      <li class="page-item"><a class="page-link" href="#">1</a></li>
-      <li class="page-item"><a class="page-link" href="#">2</a></li>
-      <li class="page-item">
-        <a class="page-link" href="#" aria-label="Next">
-          <span aria-hidden="true">Next</span>
-        </a>
-      </li>
-    </ul>
-  </nav>
 </template>
 
